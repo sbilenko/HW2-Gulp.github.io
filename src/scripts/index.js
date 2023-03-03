988e95cfb3939b49f843d40819dcbb99c09523cd
Oleksandr Bilenko <sbilenkooo@gmail.com>
2023-03-03 17:32:25 +0200

    Test 7

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,9 @@
 const burger = document.querySelector('.header__burger')
 const nav = document.querySelector('.header__nav')
+const body = document.querySelector('body')
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active')
     nav.classList.toggle('active')
+    body.classList.toggle('lock')
 })
